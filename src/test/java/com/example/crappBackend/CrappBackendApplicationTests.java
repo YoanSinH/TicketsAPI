3e070d95d184263ce11089e90e0decc170287fa2
yoansinh <joancifuentes@outlook.es>
2022-05-04 01:01:43 -0500

    main test

--- src/test/java/com/example/crappBackend/CrappBackendApplicationTests.java
+++ src/test/java/com/example/crappBackend/CrappBackendApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.crappBackend;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CrappBackendApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
-
 }
